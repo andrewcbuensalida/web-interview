92f914b45ad438d01818a7ca4bb16ac321d7c5ac
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-12-20 09:42:55 -0800

    resetting form after submission complete. just need validation

--- src/App.js
+++ src/App.js
@@ -45,20 +45,9 @@ class App extends Component {
     this.setState({ selectedConsultantType: e.target.innerText.toLowerCase() });
   }
 
-  //   ### `POST /appointments`
-
-  // Expects an object in the request's body with the following information:
-
-  // - `userId`: `Int` The user's ID that this appointment is for.
-  // - `dateTime`: The user's selected time slot as a `Date ISO 8601`.
-  // - `notes`: `String` Any notes the user added when booking. _note_: not required, can be null.
-  // - `type`: `String` The type of the appointment like `GP appointment` or `Physio appointment`.
-
-  // The endpoint responds with a 200 status on success.
-
   async handleSubmit() {
     try {
-      await fetch(`${API_ENDPOINT}/appointments`, {
+      let response = await fetch(`${API_ENDPOINT}/appointments`, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
@@ -72,10 +61,19 @@ class App extends Component {
         body: JSON.stringify({
           notes: this.state.notes,
           userId: this.state.user.id,
-          consultantType: this.state.selectedConsultantType,
+          consultantType: this.state.selectedConsultantType + ' appointment',
           appointmentType: this.state.selectedAppointmentType,
-          dateTime:this.state.selectedDateTime,
+          dateTime: this.state.selectedDateTime,
         }), // body data type must match "Content-Type" header
+      });
+      console.log(`This is response`);
+      console.log(response);
+
+      this.setState({
+        selectedConsultantType: 'gp',
+        selectedDateTime: '',
+        selectedAppointmentType: '',
+        notes: '',
       });
     } catch (err) {}
   }
