26d09bf66b0a4ed9c112f1bfa8d9763963f8f49e
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-12-20 09:26:05 -0800

    booking working with fetch api

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ import { node } from 'prop-types';
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       user: {},
       selectedConsultantType: 'gp',
       availableSlots: [],
-      selectedSlotID: null,
+      selectedDateTime: '',
       selectedAppointmentType: '',
+      notes: '',
     };
     // this.handleSelectConsultantType = this.handleSelectConsultantType.bind(this);
   }
@@ -43,6 +43,41 @@ class App extends Component {
   // second method to update selectedConsultantType
   handleSelectConsultantType(e) {
     this.setState({ selectedConsultantType: e.target.innerText.toLowerCase() });
+  }
+
+  //   ### `POST /appointments`
+
+  // Expects an object in the request's body with the following information:
+
+  // - `userId`: `Int` The user's ID that this appointment is for.
+  // - `dateTime`: The user's selected time slot as a `Date ISO 8601`.
+  // - `notes`: `String` Any notes the user added when booking. _note_: not required, can be null.
+  // - `type`: `String` The type of the appointment like `GP appointment` or `Physio appointment`.
+
+  // The endpoint responds with a 200 status on success.
+
+  async handleSubmit() {
+    try {
+      await fetch(`${API_ENDPOINT}/appointments`, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json',
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({
+          notes: this.state.notes,
+          userId: this.state.user.id,
+          consultantType: this.state.selectedConsultantType,
+          appointmentType: this.state.selectedAppointmentType,
+          dateTime:this.state.selectedDateTime,
+        }), // body data type must match "Content-Type" header
+      });
+    } catch (err) {}
   }
 
   render() {
@@ -91,7 +126,7 @@ class App extends Component {
             <img src={logo} className="app-logo" alt="Babylon Health" />
           </div>
           <h1>New Appointment</h1>
-          <div className='user'>
+          <div className="user">
             <img src={this.state.user.avatar} />
             {this.state.user.firstName} {this.state.user.lastName}
           </div>
@@ -126,10 +161,10 @@ class App extends Component {
                         <li
                           key={slot.id}
                           className={`button ${
-                            this.state.selectedSlotID === slot.id && 'selected'
+                            this.state.selectedDateTime === slot.time && 'selected'
                           }`}
                           onClick={() => {
-                            this.setState({ selectedSlotID: slot.id });
+                            this.setState({ selectedDateTime: slot.time });
                           }}
                         >
                           {moment(slot.time).format('hh:mm')}
@@ -156,13 +191,17 @@ class App extends Component {
           </div>
           <div>
             <h3>Notes</h3>
-            <textarea placeholder="Describe your symptoms" />
+            <textarea
+              placeholder="Describe your symptoms"
+              value={this.state.notes}
+              onChange={(e) => this.setState({ notes: e.target.value })}
+            />
           </div>
           <div>
             <div
               className="button"
               onClick={() => {
-                /* TODO: submit the data */
+                this.handleSubmit();
               }}
             >
               Book Consultant
