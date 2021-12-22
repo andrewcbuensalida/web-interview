537886ed5372fcb7fba880cc2c3e79e6454e4265
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-12-22 12:33:10 -0800

    separated navbar, create appointment into their own files

---
+++ client/src/components/Nav.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Nav.scss'
+
+function Nav() {
+  return (
+    <div className="navContainer">
+      <div className="navBlock"></div>
+      <ul id="nav2">
+        <div className="icon">
+          <i className="fa fa-2x fa-chevron-right"></i>
+        </div>
+        <Link to="/scheduledappointments">
+          <li className="scheduledNav">Scheduled appointments</li>
+        </Link>
+        <Link to="/createappointment">
+          <li className="createNav">Create an appointment</li>
+        </Link>
+      </ul>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ client/src/routes/ScheduledAppointments.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ScheduledAppointments() {
+    return (
+        <div>
+            <h1>Scheduled Appointments</h1>
+        </div>
+    )
+}
+
+export default ScheduledAppointments
